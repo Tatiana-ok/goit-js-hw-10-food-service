28261d9c3c3d5bf5d0b9608bb802f27c0f5d0e9e
Tetiana Pomazunovska <my.mail.tetiana@gmail.com>
2020-11-26 00:55:39 +0200

    Commit

--- index.js
+++
@@ -1,7 +0,0 @@
-import menuTemplate from './templates/menu.hbs';
-import menuItems from './menu.json';
-const menuRef = document.querySelector('js-menu');
-
-const markup = menuTemplate(menuItems);
-menuRef.insertAdjacentHTML('beforeend', markup);
-
